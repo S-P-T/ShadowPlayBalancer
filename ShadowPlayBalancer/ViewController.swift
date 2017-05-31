5565917cd227bd10965665601d15f8e654c66c20
Desperado <flamesdesperado@gmail.com>
2017-06-01 00:33:48 +0800

    Initial Commit

---
+++ ShadowPlayBalancer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ShadowPlayBalancer
+//
+//  Created by Desperado on 01/06/2017.
+//  Copyright © 2017 Des. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
